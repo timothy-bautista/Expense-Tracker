ff59ad61244623affb550794c01df50ec8de1da0
Timothy <bautistatimothy341@gmail.com>
2026-02-20 09:10:16 +0800

    Add list

---
+++ src/Helper.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Helper {
+    static int idGenerator(){
+        String currentLine;
+        String lastLine = null;
+        try(BufferedReader br = new BufferedReader(new FileReader("data.txt"))){
+            while((currentLine = br.readLine()) != null){
+                lastLine = currentLine;
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        if(lastLine != null){
+            String[] parts = lastLine.split(",");
+            return Integer.parseInt(parts[0]);
+        } else {
+            System.err.println("No such file or directory");
+            return 0;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args){
         parse(args, args.length);
@@ -13,7 +16,7 @@ public class Main {
                 add(args);
             }
             case "list" -> {
-//                list method
+                list();
             }
             case "delete" -> {
 //                delete method
@@ -24,9 +27,14 @@ public class Main {
         }
     }
 
+
+
     static void add(String[] args){
+        int currentId = Helper.idGenerator();
         String description = "";
         double amount = 0.0;
+        String filename = "data.txt";
+        LocalDate date = LocalDate.now();
 
         for(int i = 0; i < args.length; i++){
             if(args[i].equals("--description")){
@@ -36,8 +44,26 @@ public class Main {
                 amount = Double.parseDouble(args[++i]);
             }
         }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))){
+            bw.write(++currentId + "," + date + "," + description + "," + amount + "\n");
+            System.out.println("Expense added successfully (ID: " + currentId + ")");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     static void list(){
+        try(BufferedReader br = new BufferedReader(new FileReader("data.txt"))){
+            System.out.println("ID\tDate\t\tDescription\tAmount");
+            String list;
+            while((list = br.readLine()) != null){
+                String[] data = list.split(",");
+                System.out.println(data[0] + "\t" + data[1] + "\t" + data[2] + "\t\t₱" + data[3]);
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
     static void delete(){
     }
